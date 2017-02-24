9ba5ee34dc577093ba62599b53ed9cccd62253ad
Ryan Haskell-Glatz <ryan.nhg@gmail.com>
2017-02-23 23:17:38 -0600

    add professional detail page

--- routes.js
+++ routes.js
@@ -5,10 +5,14 @@ exports = module.exports = function(routes) {
 		// VIEWS
 		app.get('/', routes.views['home'])
 
+		// Landing Pages
 		app.get('/search', routes.views['search'])
 		app.get('/professionals', routes.views['search'])
 		app.get('/services', routes.views['search'])
 		app.get('/insights', routes.views['search'])
+
+		// Detail Pages
+		app.get('/lawyers/:lawyerSlug', routes.views['professional'])
 
 		// API
 		app.get('/api/search', routes.api['search'])
---
+++ routes/views/professional.js
@@ -0,0 +1,57 @@
+var keystone = require('keystone'),
+    Lawyer = keystone.list('Lawyer')
+
+module.exports = function(req, res) {
+
+    var view = new keystone.View(req, res),
+        locals = res.locals,
+        lawyerSlug = req.params.lawyerSlug
+
+    getProfessional(lawyerSlug).then( (professional) => {
+
+        if (professional === undefined) {
+
+            res.redirect('/not-found')
+
+        } else {
+
+            locals.professional = professional
+
+            view.render('professionals/index')
+
+        }
+
+    }).catch( function(reason) {
+
+        console.error(reason)
+
+        res.redirect('/not-found')
+
+    })
+
+}
+
+function getProfessional (slug) {
+
+    return Lawyer.model
+        .find( { slug: slug })
+        .limit(1)
+        .populate('office title industries practices')
+        .exec()
+        .then( function (lawyers) {
+
+            if (lawyers.length === 0) {
+
+                console.error(`Lawyer '${slug}' not found.`)
+
+                return undefined
+
+            } else {
+
+                return lawyers[0]
+
+            }
+
+        })
+
+}
